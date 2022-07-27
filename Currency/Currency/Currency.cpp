ebb99d10ff76fd3ff0ad9b29910342a0ac60a493
Peter Janssen <32429237+PeterJanssen@users.noreply.github.com>
2022-07-27 09:48:36 +0200

    Update Currency.cpp
    Add dollar cents converter to dollars, quarters, dimes and nickels
    Add euro cents converter to euros, fifty, twenty, ten, five and two cents

--- Currency/Currency/Currency.cpp
+++ Currency/Currency/Currency.cpp
@@ -6,16 +6,77 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	int dollar_cents{};
+	const int dollar_value{ 100 };
+	const int quarter_value{ 25 };
+	const int dimes_value{ 10 };
+	const int nickles_value{ 5 };
+	cout << "===============================DOLLARS===============================";
+	cout << "\nEnter an amount in dollar cents: ";
+
+	cin >> dollar_cents;
+
+	int balance = dollar_cents;
+
+	int dollars = balance / dollar_value;
+	balance = dollar_cents % dollar_value;
+
+	int quarters = balance / quarter_value;
+	balance %= quarter_value;
+
+	int dimes = balance / dimes_value;
+	balance %= dimes_value;
+
+	int nickles = balance / nickles_value;
+	balance %= nickles_value;
+
+	int pennies = balance;
+
+	cout << "You can provide change for this change as follows: "
+		<< "\ndollars: " << dollars << "\nquarters: " << quarters << "\ndimes: " << dimes << "\nnickles: " << nickles
+		<< "\npennies: " << pennies;
+
+	cout << "\n===============================EUROS===============================\n";
+
+	int euro_cents{};
+	const int one_euro_value{ 100 };
+	const int fifty_cent_value{ 50 };
+	const int twenty_cent_value{ 20 };
+	const int ten_cent_value{ 10 };
+	const int five_cent_value{ 5 };
+	const int two_cent_value{ 2 };
+
+	cout << "Enter an amount in euro cents: ";
+
+	cin >> euro_cents;
+
+	balance = euro_cents;
+
+	int one_euros = balance / one_euro_value;
+	balance = euro_cents % one_euro_value;
+
+	int fifty_cents = balance / fifty_cent_value;
+	balance %= fifty_cent_value;
+
+	int twenty_five_cents = balance / twenty_cent_value;
+	balance %= twenty_cent_value;
+
+	int ten_cents = balance / ten_cent_value;
+	balance %= ten_cent_value;
+
+	int five_cents = balance / five_cent_value;
+	balance %= five_cent_value;
+
+	int two_cents = balance / two_cent_value;
+	balance %= two_cent_value;
+
+	int one_cents = balance;
+
+	cout << "You can provide change for this change as follows: "
+		<< "\none euro's: " << one_euros << "\nfifty cents: " << fifty_cents
+		<< "\ntwenty cents: " << twenty_five_cents << "\nten cents: " << ten_cents << "\nfive cents: " << five_cents
+		<< "\ntwo cents: " << two_cents << "\none cents: " << one_cents;
+
+	cout << endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
