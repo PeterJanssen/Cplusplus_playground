ac48fc5c6717efd082933a7708a28b6a803293f2
Peter Janssen <32429237+PeterJanssen@users.noreply.github.com>
2022-07-28 20:58:52 +0200

    Update Currency.cpp
    Cleanup trailing spaces and semicolons
    Add for loop for printing currency choices
    Remove not needed printed conversions with if statement in for loop
    Print a more readable output for conversion
    Add function to catch invalid input

--- Currency/Currency/Currency.cpp
+++ Currency/Currency/Currency.cpp
@@ -2,21 +2,41 @@
 #include <vector>
 using namespace std;
 
+void printInvalidInputErrorMessage(string message) {
+	cin.clear();
+	cin.ignore(256, '\n');
+
+	cout << "==============================================================" << endl
+		<< message << endl
+		<< "==============================================================" << endl;
+}
+
 void centsToCurrencyConverter(vector<int> values, vector<string> currency_value_names) {
 	int cents{ 0 };
 
-	cout << "Enter an amount: ";
+	cout << "Enter an amount in cents: ";
 
 	cin >> cents;
 
-	int balance = cents;
-	cout << "You can provide change for this change as follows:" << endl;
+	while (cents <= 0 || cin.fail()) {
+		printInvalidInputErrorMessage("Please enter a number above 0");
+		cout << "Enter an amount in cents: ";
+		cin >> cents;
+	}
 
-	for (int i = 0; i < values.size(); i++)
+	int balance = cents;
+	int currency{ 0 };
+
+	cout << "You can provide change for " << balance << " as follows : " << endl;
+
+	for (int i{ 0 }; i < values.size(); i++)
 	{
-		int currency = balance / values[i];
-		balance %= values[i];
-		cout << currency_value_names[i] << ": " << currency << endl;
+		currency = balance / values.at(i);
+		cents = balance;
+		balance %= values.at(i);
+		if (currency > 0) {
+			cout << cents - balance << "\tconverted to\t" << currency << "*\t" << currency_value_names.at(i) << endl;
+		}
 	}
 }
 
@@ -24,11 +44,18 @@ void printCurrencyNameBanner(string currency_name) {
 	cout << "===============================" << currency_name << "===============================" << endl;
 }
 
+void printCurrencyChoices(vector<string> currency_names) {
+	for (int i{ 1 }, j{ 0 }; j < currency_names.size(); i++, j++) {
+		cout << i << ". " << currency_names.at(j) << endl;
+	}
+
+	cout << "0. to exit application" << endl;
+}
 
 int main()
 {
 	vector<int> dollar_values = { 10000,5000,2000,1000,100,25,10,5,1 };
-	vector <string> dollar_values_names = {
+	vector<string> dollar_values_names = {
 		"hundred dollars",
 		"fifty dollars",
 		"twenty dollars",
@@ -41,7 +68,7 @@ int main()
 	};
 
 	vector<int> euro_values = { 50000,20000,10000,5000,2000,1000,500,100,50,20,10,5,2,1 };
-	vector <string> euro_values_names = {
+	vector<string> euro_values_names = {
 		"five hundred euro's",
 		"two hundred euro's",
 		"one hundred euro's",
@@ -58,37 +85,37 @@ int main()
 		"one cents"
 	};
 
+	vector<string> currency_names{ "DOLLAR","EURO" };
+
 	bool exiting_application = false;
 
 	char input{ '0' };
 
 	while (exiting_application == false) {
 		cout << "Please select a currency you would like to convert." << endl;
-		cout << "1. dollars" << endl;;
-		cout << "2. euro's" << endl;
-		cout << "0. to exit application" << endl;
+
+		printCurrencyChoices(currency_names);
+
 		cin >> input;
 
 		switch (input)
 		{
 		case '1':
-			printCurrencyNameBanner("DOLLAR");
+			printCurrencyNameBanner(currency_names.at(0));
 			centsToCurrencyConverter(dollar_values, dollar_values_names);
-			printCurrencyNameBanner("DOLLAR");
+			printCurrencyNameBanner(currency_names.at(0));
 			break;
 		case '2':
-			printCurrencyNameBanner("EURO'S");
+			printCurrencyNameBanner(currency_names.at(1));
 			centsToCurrencyConverter(euro_values, euro_values_names);
-			printCurrencyNameBanner("EURO'S");
+			printCurrencyNameBanner(currency_names.at(1));
 			break;
 		case '0':
 			cout << "Exiting application." << endl;
 			exiting_application = true;
 			break;
 		default:
-			cout << "==============================================================" << endl;
-			cout << "Please provide a valid input." << endl;
-			cout << "==============================================================" << endl;
+			printInvalidInputErrorMessage("Please provide a valid input.");
 			break;
 		}
 	}
